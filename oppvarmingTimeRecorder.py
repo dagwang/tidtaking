3788ab822f3f09cb961d1c9cc9f456f598c4c37e
dagwa <dag.wang@vikenfiber.no>
2019-10-20 21:26:07 +0200

    Fremdeles bare oppvarming til time recorder

--- oppvarmingTimeRecorder.py
+++ oppvarmingTimeRecorder.py
@@ -11,11 +11,18 @@ server = 'BASE'
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 sqlcursor = cnxn.cursor()
 
+def fraction_of_day(dt):
+    midnight = datetime.datetime.combine(datetime.date.today(), datetime.time(0))
+    seconds_in_day = 24 * 3600
+    if isinstance(dt,datetime.datetime):
+        dt = dt - midnight
+    return (dt.seconds+dt.microseconds*10**-6)/seconds_in_day
+
 
 def get_runner(startno):
-    sqlcursor.execute("SELECT name.starttime , name.name , name.ename , name.intime FROM name WHERE name.startno =" + str(startno) )
+    sqlcursor.execute("SELECT name.startno, name.starttime , name.name , name.ename , name.intime FROM name WHERE name.startno =" + str(startno) )
     runner = sqlcursor.fetchall();
-    return runner
+    return runner[0]
 
 def set_in_time(startno,in_time):
     sqlcursor.execute(
@@ -28,19 +35,26 @@ def set_in_time(startno,in_time):
 
 text_elem = sg.Text("", size=(18, 1))
 startinput = sg.Input()
-
-layout = [[sg.Text("Skriv inn startnummer   (F1 tar tiden)")],
-          [startinput],
-          #[text_elem],
-          [sg.Button("Avslutt")]]
-
-input_window = sg.Window("Database: " + database, layout, return_keyboard_events=True, use_default_focus=True)
-
 highest_runner = 0
 highest_time = 0
 highest = 0
+runner_array =[]
+time_array=[]
+in_time_array =[]
 lines_to_display = 10
+for ii in range(lines_to_display):
+    runner_array.append(sg.Text('',size=(18, 1)))
+    time_array.append(sg.Text(''))
 
+layout = [[sg.Text("Skriv inn startnummer   (F1 tar tiden)")],
+          [startinput]]
+layout += [[runner_array[ii],time_array[ii]] for ii in range(lines_to_display)]
+layout += [[sg.Button("Avslutt")]]
+
+input_window = sg.Window("Database: " + database, layout, return_keyboard_events=True, use_default_focus=True)
+
+runner_list = []
+time_list = []
 # ---===--- Loop taking in user input --- #
 while True:
     event, value = input_window.Read()
@@ -51,17 +65,31 @@ while True:
     #if text_elem.DisplayText == 'F1:112':
     if event == 'F1:112':
         now = datetime.datetime.now()
+        time_list.append(now)
         time_string = now.strftime("%H:%M:%S")
-        in_time_array[highest_time] = time_string
-        highest_time += 1
-        highest = max(highest_time,highest_time)
+        in_time_array.append(time_string)
+        #time_array[highest_time].update(time_string)
         print(time_string)
+        if highest_time < highest_runner:    # Start number already entered update time
+            this_runner = runner_list[highest_time]
+            print(this_runner)
+            start_number = this_runner.startno
+            if this_runner.intime is None:
+                set_in_time(this_runner.startno,fraction_of_day(now))
+                print('Updating: ', this_runner)
+        highest_time += 1
+
     if ord(event[0]) == 13:
         runner = get_runner(value[0])
-        runner_array[highest_runner] = runner
+        runner_list.append(runner)
+        if highest_time > highest_runner:    # Start time already entered update time
+            this_time = time_list[highest_runner]
+            if runner.intime is None:
+                set_in_time(runner.startno, fraction_of_day(now))
+                print('updating:', runner)
         highest_runner += 1
         startinput.update('')
-        highest = max(highest_time, highest_time)
+        highest = max(highest_time, highest_runner)
         print(runner)
     if event == "Avslutt" or event is None:
         print(event, "exiting")
