774e5898d1faf28e0a50223955f0e19043d28356
dagwa <dag.wang@vikenfiber.no>
2019-10-20 22:08:48 +0200

    Fremdeles bare oppvarming til time recorder

--- oppvarmingTimeRecorder.py
+++ oppvarmingTimeRecorder.py
@@ -35,8 +35,8 @@ def set_in_time(startno,in_time):
 
 text_elem = sg.Text("", size=(18, 1))
 startinput = sg.Input()
-highest_runner = 0
-highest_time = 0
+next_runner = 0
+next_time = 0
 highest = 0
 runner_array =[]
 time_array=[]
@@ -44,7 +44,7 @@ in_time_array =[]
 lines_to_display = 10
 for ii in range(lines_to_display):
     runner_array.append(sg.Text('',size=(18, 1)))
-    time_array.append(sg.Text(''))
+    time_array.append(sg.Text('',size=(10, 1)))
 
 layout = [[sg.Text("Skriv inn startnummer   (F1 tar tiden)")],
           [startinput]]
@@ -58,39 +58,46 @@ time_list = []
 # ---===--- Loop taking in user input --- #
 while True:
     event, value = input_window.Read()
-
     # text_elem.Update(event)
     print(event, ord(event[0]) , value)
     print(text_elem.DisplayText)
     #if text_elem.DisplayText == 'F1:112':
-    if event == 'F1:112':
+    if event == 'F1:112':                              # A time should be recorded
         now = datetime.datetime.now()
         time_list.append(now)
         time_string = now.strftime("%H:%M:%S")
         in_time_array.append(time_string)
-        #time_array[highest_time].update(time_string)
         print(time_string)
-        if highest_time < highest_runner:    # Start number already entered update time
-            this_runner = runner_list[highest_time]
+        if next_time < next_runner:    # Start number already entered update time
+            this_runner = runner_list[next_time]
             print(this_runner)
             start_number = this_runner.startno
             if this_runner.intime is None:
                 set_in_time(this_runner.startno,fraction_of_day(now))
                 print('Updating: ', this_runner)
-        highest_time += 1
-
-    if ord(event[0]) == 13:
+        next_time += 1
+        highest = max(next_time, next_runner)-1
+    elif ord(event[0]) == 13:                     # A start number has been entered
         runner = get_runner(value[0])
         runner_list.append(runner)
-        if highest_time > highest_runner:    # Start time already entered update time
-            this_time = time_list[highest_runner]
+        if next_time > next_runner:    # Start time already recorded - update time
+            this_time = time_list[next_runner]
             if runner.intime is None:
                 set_in_time(runner.startno, fraction_of_day(now))
                 print('updating:', runner)
-        highest_runner += 1
+        next_runner += 1
         startinput.update('')
-        highest = max(highest_time, highest_runner)
+        highest = max(next_time, next_runner)
         print(runner)
+    for line_number in range(lines_to_display):
+        if 0 <= highest-line_number < next_time:
+            time_array[line_number].update(in_time_array[highest-line_number])
+        else:
+            time_array[line_number].update('')
+        if 0 <= highest-line_number < next_runner:
+            runner_array[line_number].update(runner_list[highest-line_number].name)
+        else:
+            runner_array[line_number].update('')
     if event == "Avslutt" or event is None:
         print(event, "exiting")
         break
